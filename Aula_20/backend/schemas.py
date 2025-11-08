42ea2527ffa959a7db373f440600caf2e862972f
Thiago Souza <thiagolsmail@gmail.com>
2025-11-08 20:32:49 -0300

    Aula 20 - backend concluido

---
+++ Aula_20/backend/schemas.py
@@ -0,0 +1,55 @@
+from datetime import datetime
+from enum import Enum
+from typing import Optional
+
+from pydantic import BaseModel, PositiveFloat, EmailStr, field_validator
+
+
+class CategoriaBase(Enum):
+    categoria1 = "Eletrônico"
+    categoria2 = "Eletrodoméstico"
+    categoria3 = "Móveis"
+    categoria4 = "Roupas"
+    categoria5 = "Calçados"
+
+
+class ProductBase(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: PositiveFloat
+    categoria: str
+    email_fornecedor: EmailStr
+
+    @field_validator("categoria")
+    def check_categoria(cls, v):
+        if v in [item.value for item in CategoriaBase]:
+            return v
+        raise ValueError("Categoria inválida")
+
+
+class ProductCreate(ProductBase):
+    pass
+
+
+class ProductResponse(ProductBase):
+    id: int
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
+
+
+class ProductUpdate(BaseModel):
+    name: Optional[str] = None
+    description: Optional[str] = None
+    price: Optional[PositiveFloat] = None
+    categoria: Optional[str] = None
+    email_fornecedor: Optional[EmailStr] = None
+
+    @field_validator("categoria", pre=True, always=True)
+    def check_categoria(cls, v):
+        if v is None:
+            return v
+        if v in [item.value for item in CategoriaBase]:
+            return v
+        raise ValueError("Categoria inválida")
